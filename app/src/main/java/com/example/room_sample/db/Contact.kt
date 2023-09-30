6478aa683461145defc2d3434e67dc5d13aba5cf
Arjun <71964107+arjun-sasidharan@users.noreply.github.com>
2023-09-30 13:08:28 +0530

    Add room entity class

---
+++ app/src/main/java/com/example/room_sample/db/Contact.kt
@@ -0,0 +1,18 @@
+package com.example.room_sample.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contact_data_table")
+data class Contact(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "contact_id")
+    val id: Int,
+
+    @ColumnInfo(name = "contact_name")
+    val name: String,
+
+    @ColumnInfo(name = "contact_phone_no")
+    val phoneNo: String
+)
